c521f0a17c503c375677ac6de2bfbc09a404ec18
simraan18 <simraan.khan@evolvextechnology.com>
2026-07-15 10:34:17 +0400

    feat: handle unauthorized request

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <RouterProvider router={router} />
-        <Toaster position="bottom-right" richColors />
+        <Toaster position="top-right" richColors />
       </PersistGate>
     </Provider>
   </StrictMode>,
--- src/store/service/baseQuery.ts
+++ src/store/service/baseQuery.ts
@@ -1,5 +1,7 @@
 import { type BaseQueryFn, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "../store";
+import { routePath } from "@/routes/route-path";
+import { toast } from "sonner";
 
 export const baseQuery: BaseQueryFn = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_API_BASE_URL,
@@ -11,5 +13,15 @@ export const baseQuery: BaseQueryFn = fetchBaseQuery({
     }
 
     return headers;
+  },
+  async responseHandler(response) {
+    if (response.status === 403) {
+      toast.warning("Unauthorized");
+      localStorage.clear();
+      window.location.href = routePath.login;
+      return null;
+    }
+    const responseData = await response.json();
+    return responseData;
   },
 });
